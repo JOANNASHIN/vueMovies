88c7ff8f90cc721ea0cf4054a5e4386aa9f09b43
jieunshin <joannashin3@forbiz.co.kr>
2021-11-11 18:08:12 +0900

    검색 css 수정

--- vue-movies/src/components/HeaderComponent.vue
+++ vue-movies/src/components/HeaderComponent.vue
@@ -1,24 +1,16 @@
 <template>
     <header class="fb__header">
-        <!-- <router-link to="/">Home</router-link> -->
-<!-- 
-        <nav id="nav" class="fb__nav">
-            <ul class="fb__nav__wrapper">
-                <li class="fb__nav__menu" v-for="(menu, index) in menus" :key="index" :class="$route.params.id == menu.id ? 'active' : ''">
-                    <router-link :to="`/detail/${menu.id}`">
-                        <span>{{ menu.name }}</span>
-                    </router-link>
-                </li>
-            </ul>
-        </nav> -->
-
         <form action="" class="fb__search" @submit.prevent="searchInit($event)">
             <fieldset class="fb__search__wrapper">
                 <legend>검색하기</legend>
                 <input type="search" class="fb__search__input" placeholder="Search" v-model.trim="searchWord" @keyup.stop="searchInit($event)">
-                <button type="submit" class="fb__search__button">Search</button>
             </fieldset>
         </form>
+
+        <!-- <nav class="fb__header__floating" :class="isFold ? 'active' : ''">
+            <button class="fb__header__menu filter" @click="openFilterLayer($event)">필터 버튼</button>
+            <button class="fb__header__fold" @click="isFold = !isFold">접기 버튼</button>
+        </nav> -->
     </header>
 </template>
 
@@ -28,8 +20,9 @@ export default {
     name: "Header",
     data() {
         return {
-            menus: [],
-            searchWord: ""
+            searchWord: "",
+            isFilterShow: false,
+            isFold: false,
         }
     },
     created() {
@@ -55,6 +48,10 @@ export default {
         searchInit() {
             if (!this.searchWord) return ;
             eventBus.$emit("search:tv", this.searchWord);
+        },
+
+        openFilterLayer() { 
+            eventBus.$emit("openFilterLayer");
         }
     }
 }
